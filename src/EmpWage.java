e98827203e81a708129d97f484ab62518d07d5c6
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-06-18 19:07:28 +0530

    Modified to the Random Present and Absent of an employee and hours

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,12 +1,19 @@
 public class EmpWage{
     public static void main(String[] args){
-        System.out.println(" Welcome To Employee Wage");
         int IS_FULL_TIME = 1;
+        int Wage_per_hour = 20;
+        int Full_work_hour = 8;
+        int Emp_wage ;
         double empCheck = Math.floor(Math.random()*10)%2;
-        // checking the presence of Employee
+        // Calculating the daily wage of Employee
         if ( empCheck == IS_FULL_TIME )
-            System.out.println( " Employee is Present ");
+            //Employee is Present
+            Emp_wage = 1;
         else
-            System.out.println(" Employee is Absent ");
+            //Employee is Absent
+            Emp_wage = 0;
+
+        int Emp = Emp_wage * Wage_per_hour * Full_work_hour;
+        System.out.println("Wage of a employee per day : " + Emp);
     }
 }
