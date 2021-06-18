dc3f15991b979ac8e4232ae4d0705c3e9a0b323e
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-06-18 18:39:44 +0530

    Modified to the Random Present and Absent of an employee

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,5 +1,12 @@
-public class EmpWage {
+public class EmpWage{
     public static void main(String[] args){
         System.out.println(" Welcome To Employee Wage");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random()*10)%2;
+        // checking the presence of Employee
+        if ( empCheck == IS_FULL_TIME )
+            System.out.println( " Employee is Present ");
+        else
+            System.out.println(" Employee is Absent ");
     }
 }
