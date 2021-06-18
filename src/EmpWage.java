2fdbf0ba424ad807281c92bfc45226d29567dcef
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-06-18 20:06:30 +0530

    Modified to an employee wage for monthly

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,21 +1,31 @@
 public class EmpWage{
-    public static void main(String[] args){
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int Wage_per_hour = 20;
-        int Emp_wage_hour;
-        double empCheck = Math.floor(Math.random()*10)%3;
-        //  Part Time wagge and Full time wage
-        if ( empCheck == IS_FULL_TIME )
-            //Employee is Present
-            Emp_wage_hour = 8 ;
-        else if ( empCheck == IS_PART_TIME )
-            //Employee is Absent
-            Emp_wage_hour = 4;
-        else
-            Emp_wage_hour = 0;
-
-        int Emp = Emp_wage_hour * Wage_per_hour;
-        System.out.println("Wage of a employee per day : " + Emp);
+        public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+        public static final int Wage_per_hour = 20;
+        public static final int Total_Working_days = 20;
+        public static void main (String[] args){
+            System.out.println(" Welcome To Employee Wage");
+            int Emp_wage_hour;
+            int Monthly_wage = 0;
+            // for loop from day 1 to day 20 wage working days in month
+            for ( int day = 1; day <= Total_Working_days; day++){
+                int empCheck = (int) Math.floor(Math.random()*10)%3;
+                // checking for employe work time
+                switch (empCheck) {
+                    case IS_FULL_TIME:
+                        Emp_wage_hour = 8 ;
+                        break;
+                    case IS_PART_TIME:
+                        Emp_wage_hour = 4;
+                        break;
+                    default:
+                        Emp_wage_hour = 0;
+                }
+                //  calculation of Wage per hour and monthly wage of an Employee
+                int Emp = Emp_wage_hour * Wage_per_hour;
+                System.out.println("Wage of a employee at day "+day+"\t"+ Emp);
+                Monthly_wage += Emp;
+            }
+            System.out.println("Monthly Wages of an Employee : " + Monthly_wage);
+        }
     }
-}
