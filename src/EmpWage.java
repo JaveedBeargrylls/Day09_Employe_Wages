6a317373ccec2b717642bc19bb31f395025a955b
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-06-19 17:08:56 +0530

    Modified to an employee wage using Class Method

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,22 +1,18 @@
 public class EmpWage{
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
-    public static final int Wage_per_hour = 20;
-    public static final int Total_Working_days = 20;
-    public static final int Maximum_work_hours = 100;
-    public static void main (String[] args){
-        System.out.println(" Welcome To Employee Wage");
+
+    static void Emp_wage(int Wage_per_hour, int Total_Working_days, int Maximum_work_hours){
+        System.out.println("Welcome To Employee Wage");
         int Emp_wage_hour;
         int Monthly_wage = 0;
         int Working_days = 0, total_emp_hours = 0;
-        // working days of Employee in a month and its maximum hours of work
+        //working days of Employee in a month and its maximum hours of work
         while ( Working_days < Total_Working_days && total_emp_hours <= Maximum_work_hours){
             Working_days++;
             int empCheck = (int) Math.floor(Math.random()*10)%3;
             // checking of his work time
             Emp_wage_hour = switch (empCheck) {
-                case IS_FULL_TIME -> 8;
-                case IS_PART_TIME -> 4;
+                case 1 -> 8;
+                case 2 -> 4;
                 default -> 0;
             };
             // calculation of wage and time of an Employee in a month
@@ -25,7 +21,9 @@ public class EmpWage{
             System.out.println("Wage of a employee at day "+Working_days+"\t"+ Emp + "\t" + "Work time :"+" "+Emp_wage_hour);
             Monthly_wage += Emp;
         }
-       System.out.println("Monthly Wages of an Employee : " + Monthly_wage+"\n"+"Monthly Working Hours of an employee : "+total_emp_hours+"hours");
+        System.out.println("Monthly Wages of an Employee : " + Monthly_wage+"\n"+"Monthly Working Hours of an employee : "+total_emp_hours+"hours");
+    }
+    public static void main(String[] args){
+        Emp_wage(30,24,100);
     }
 }
-
